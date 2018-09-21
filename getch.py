4b21b7444e03bb9e5bbe3c321763164a0589529f
Nircek <nircek-2103@protonmail.com>
2018-09-21 18:49:44 +0200

    Add getch() script

---
+++ getch.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# inspired by http://code.activestate.com/recipes/134892/
+try:
+    import msvcrt
+    getch = msvcrt.getch
+except ImportError:
+    import sys, tty, termios
+    def getch():
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(sys.stdin.fileno())
+            ch = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
